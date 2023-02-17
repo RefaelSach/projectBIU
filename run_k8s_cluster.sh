45470a22c48d395837c211f18b6e1be5316bba8b
RefaelSachevski <refael32901@gmail.com>
2023-02-17 13:39:29 +0200

    Fix k8s file

--- run_k8s_cluster.sh
+++ run_k8s_cluster.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker build -t pingpong-flask-app .
+docker build -t refaelsa/pingpong:flask-app .
 minikube start
-kubectl create deployment pingpong --image=pingpong-flask-app --replicas=4
+kubectl create deployment pingpong --image=refaelsa/pingpong:flask-app --replicas=4
 kubectl expose deployment pingpong --type=LoadBalancer --port=5005 --target-port=5005
 service_url=$(minikube service pingpong --url)
 
