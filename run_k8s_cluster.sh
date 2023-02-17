9f493273885ee64f145712c6bb235e19fb0e8d6a
RefaelSachevski <refael32901@gmail.com>
2023-02-17 19:57:51 +0200

    fix

--- run_k8s_cluster.sh
+++ run_k8s_cluster.sh
@@ -6,3 +6,7 @@ kubectl create deployment pingpong --image=refaelsa/pingpong:flask-app --replica
 kubectl expose deployment pingpong --type=LoadBalancer --port=5005 --target-port=5005
 service_url=$(minikube service pingpong --url)
 
+#once the k8s is up and running. open a new terminal and run
+#minikube tunnel
+#then run the the script 
+#python3 testServer.py
