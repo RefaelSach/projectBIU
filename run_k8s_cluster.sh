2ae48f1fcde625d012033d49a662c95842e5453c
RefaelSachevski <refael32901@gmail.com>
2023-02-17 12:12:05 +0200

    k8s file

---
+++ run_k8s_cluster.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker build -t pingpong-flask-app .
+minikube start
+kubectl create deployment pingpong --image=pingpong-flask-app --replicas=4
+kubectl expose deployment pingpong --type=LoadBalancer --port=5005 --target-port=5005
+service_url=$(minikube service pingpong --url)
+
+# Test the Flask app
+curl $service_url/pingpong
+python3 testServer.py
+
